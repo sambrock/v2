c9153ccdac9a3a39e104d153cf0801a4ef596d6d
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-17 19:02:03 +0100

    Updated projects to json prop data.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 
 import ProjectSlide from './ProjectSlide';
 
-import PortfolioProjects from '../portfolio-projects.json';
+import PortfolioProjects from '../json/projects.json';
+import Pagination from './Pagination';
 
-export default function Projects() {
+export default function Projects({ data }) {
   const [projects, setProjects] = useState(null);
   // const [scroll, setScroll] = useState(null);
   const [active, setActive] = useState(0);
   const [animate, setAnimate] = useState({ index: active, type: 'in', direction: 'down' });
 
   useEffect(() => {
-    setProjects(PortfolioProjects)
-  }, [])
+    setProjects(data)
+  }, [data])
 
   const handleNavigation = (scroll) => {
     let next = active;
@@ -36,16 +36,22 @@ export default function Projects() {
   if (!projects) return <div></div>;
 
   return (
-    <ReactScrollWheelHandler className="projects" upHandler={(e) => handleNavigation('up')} downHandler={(e) => handleNavigation('down')} >
-      {projects.map((project, index) => (
-        <ProjectSlide key={project.id}
-          project={project}
-          isActive={index === active}
-          animate={animate}
-          current={active + 1}
-          count={projects.length}
-        />
-      ))}
-    </ReactScrollWheelHandler>
+    <div class="projects-container">
+      <Pagination />
+      <ReactScrollWheelHandler className="projects" upHandler={(e) => handleNavigation('up')} downHandler={(e) => handleNavigation('down')} >
+        {projects.map((projectNode, index) => {
+          const project = projectNode.node;
+          return (
+            <ProjectSlide key={project.id}
+              project={project}
+              isActive={index === active}
+              animate={animate}
+              current={active + 1}
+              count={projects.length}
+            />
+          )
+        })}
+      </ReactScrollWheelHandler>
+    </div>
   )
 }
