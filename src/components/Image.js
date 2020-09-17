63eca7eebcb18656a5b60b821872c0df096a4f3c
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-17 19:01:40 +0100

    Added graphql query for json data.

---
+++ src/components/Image.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+const Image = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                fluid(maxWidth: 1440) {
+                  srcSet
+                  srcSetWebp
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+
+    render={data => {
+
+      const image = data.images.edges.find(n => {
+        return n.node.relativePath.includes(props.filename)
+      });
+
+      return !image ? null : (
+        <Img alt={props.alt} fluid={image.node.childImageSharp.fluid} className={props.classes} imgStyle={{height: false, width: false, left: false}}/>
+      )
+    }}
+  />
+)
+export default Image
