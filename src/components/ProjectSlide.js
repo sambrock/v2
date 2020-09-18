07ce466b8559cfc10f9a54a21a857258cc11df12
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-18 20:36:30 +0100

    Added hover animations.

--- src/components/ProjectSlide.js
+++ src/components/ProjectSlide.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useWindowSize } from '../hooks/window-hooks';
-import { projectSlideAnimateIn, projectSlideAnimateOut } from '../animations/animations';
-import Image from './Image';
+import { projectSlideAnimateIn, projectSlideAnimateOut, projectSlideAnimateHoverIn, projectSlideAnimateHoverOut } from '../animations/animations';
+import Image from './image';
 
 import LogoT from '../images/logo-g-t.svg';
 import LogoWT from '../images/logo-w-t.svg';
@@ -10,24 +10,27 @@ export default function ProjectSlide({ project, isActive, animate, current, indi
   const [width, height] = useWindowSize();
 
   useEffect(() => {
+    projectSlideAnimateHoverOut().play();
+    
     if (animate.type === 'in') projectSlideAnimateIn(animate.direction).play();
     if (animate.type === 'out') projectSlideAnimateOut(animate.direction).play();
   }, [isActive, animate])
 
+
   return (
-    <div className={`project-slide ${isActive ? 'active' : ''}`}>
+    <div className={`project-slide ${isActive ? 'active' : ''}`} >
       <div className="project-slide__logo"><img src={LogoT} /></div>
       <div className="project-slide__mask" style={{ height: `${height}px` }}>
         <div className="project-slide__logo"><img src={LogoWT} /></div>
-        <div className="project-slide__bg" style={{ background: `${project.color}` }}>
+        <div className="project-slide__bg" style={{ background: `${project.color}` }} onMouseEnter={() => projectSlideAnimateHoverIn().play()} onMouseLeave={() => projectSlideAnimateHoverOut().play()}>
           {project.type === 'web' && (
-            <Image filename={project.projectSlideImg} alt={project.title} classes="project-slide__img" />
+            <Image filename={project.projectSlideImg} alt={project.title} classes="project-slide__img web" />
           )}
           {project.type === 'phone' && (
             <div className="project-slide__img phone">
               <Image filename={project.projectSlideImg + '-1.png'} alt={project.title} />
-              <Image filename={project.projectSlideImg + '-2.png'} classes={" phone-2"} alt={project.title} />
-              <Image filename={project.projectSlideImg + '-3.png'} classes={" phone-3"} alt={project.title} />
+              <Image filename={project.projectSlideImg + '-2.png'} alt={project.title} />
+              <Image filename={project.projectSlideImg + '-3.png'} alt={project.title} />
             </div>
           )}
           <div className="project-slide__info">
@@ -36,7 +39,7 @@ export default function ProjectSlide({ project, isActive, animate, current, indi
             <div className="project-slide__count"><span className="count">{(current.toString().length < 2) ? `0${current}` : current}.</span></div>
             <div className={`scroll-indicator ${indicator ? '' : 'hide'}`}>
               <span>SCROLL</span>
-              <div class="indicator"><span></span></div>
+              <div className="indicator"><span></span></div>
             </div>
           </div>
         </div>
