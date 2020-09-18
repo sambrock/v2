30561e7707221a11807b0d0b38b743c90ce866a8
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-18 20:35:43 +0100

    Added layout with header.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,30 @@
 import { graphql } from 'gatsby';
-import React from 'react'
-import Header from '../components/Header';
-import Projects from '../components/Projects';
-import SocialLinks from '../components/SocialLinks';
+import React, { useState } from 'react'
+import Layout from '../components/layouts/layout';
+import Projects from '../components/projects';
+import SocialLinks from '../components/socialLinks';
 
 import '../styles/main.scss';
 
 export default function Index({ data }) {
+  const [accentColor, setAccentColor] = useState();
+
   return (
-    <main>
-      <Header />
-      <div className="container">
-        <section className="overview">
-          <div className="intro">
-            <h1>Hi, <br /> I'm Sam, <br /> web developer.</h1>
-            <p>I use modern web technologies to create engaging online
-            experiences. I'm currently a Web Technologies student at the University of
+    <Layout>
+        <div className="container">
+          <section className="overview">
+            <div className="intro">
+              <h1>Hi, <br /> I'm Sam, <br /> web developer.</h1>
+              <p>I use modern web technologies to create engaging online
+              experiences. I'm currently a Web Technologies student at the University of
             Huddersfield, graduating in 2021. </p>
-          </div>
-          <SocialLinks />
-        </section>
-        <Projects data={data.projects.edges} />
-      </div>
-    </main>
+              <a className="btn primary-bg" style={{ color: accentColor, border: `1px solid ${accentColor}` }} href="mailto:sambrocklehurst1998@gmail.com">Get in touch</a>
+            </div>
+            <SocialLinks />
+          </section>
+          <Projects data={data.projects.edges} setAccent={(color) => setAccentColor(color)} />
+        </div>
+    </Layout>
   )
 }
 
