04f712730a672576e55cc690be5735a636e4ea39
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-22 00:06:34 +0100

    Added transition link wrapper.

--- src/components/project-slide.js
+++ src/components/project-slide.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'gatsby';
 import { useWindowSize } from '../hooks/window-hooks';
-import { projectSlideAnimateIn, projectSlideAnimateOut, projectSlideAnimateHoverIn, projectSlideAnimateHoverOut } from '../animations/animations';
+import { projectSlideAnimateIn, projectSlideAnimateOut, projectSlideAnimateHoverIn, projectSlideAnimateHoverOut, projectSlideLinkAnimate } from '../animations/animations';
 import Image from './image';
+import TransitionLink from "gatsby-plugin-transition-link";
 
 import LogoT from '../images/logo-g-t.svg';
 import LogoWT from '../images/logo-w-t.svg';
@@ -18,32 +20,34 @@ export default function ProjectSlide({ project, isActive, animate, current, indi
 
 
   return (
-    <div className={`project-slide ${isActive ? 'active' : ''}`} >
-      <div className="project-slide__logo"><img src={LogoT} /></div>
-      <div className="project-slide__mask" style={{ height: `${height}px` }}>
-        <div className="project-slide__logo"><img src={LogoWT} /></div>
-        <div className="project-slide__bg" style={{ background: `${project.color}` }} onMouseEnter={() => projectSlideAnimateHoverIn().play()} onMouseLeave={() => projectSlideAnimateHoverOut().play()}>
-          {project.type === 'web' && (
-            <Image filename={`project-slide__${project.title}.png`} alt={project.title} classes="project-slide__img web" />
-          )}
-          {project.type === 'phone' && (
-            <div className="project-slide__img phone">
-              <Image filename={`project-slide__${project.title}-1.png`} alt={project.title} />
-              <Image filename={`project-slide__${project.title}-2.png`} alt={project.title} />
-              <Image filename={`project-slide__${project.title}-3.png`} alt={project.title} />
-            </div>
-          )}
-          <div className="project-slide__info">
-            <div className="project-slide__short-desc to-animate">{project.shortDescription}</div>
-            <div className="project-slide__title to-animate">{project.title}.</div>
-            <div className="project-slide__count"><span className="count">{(current.toString().length < 2) ? `0${current}` : current}.</span></div>
-            <div className={`scroll-indicator ${indicator ? '' : 'hide'}`}>
-              <span>SCROLL</span>
-              <div className="indicator"><span></span></div>
+      <TransitionLink to={`/${project.title.toLowerCase()}`} exit={{ trigger: ({exit, node}) => projectSlideLinkAnimate(height, width).play(), length: 1}} entry={{delay: 1}} >
+        <div className={`project-slide ${isActive ? 'active' : ''}`}>
+          <div className="project-slide__logo"><img src={LogoT} /></div>
+          <div className="project-slide__mask" style={{ height: `${height}px` }}>
+            <div className="project-slide__logo"><img src={LogoWT} /></div>
+            <div className="project-slide__bg" style={{ background: `${project.color}` }} onMouseEnter={() => projectSlideAnimateHoverIn().play()} onMouseLeave={() => projectSlideAnimateHoverOut().play()}>
+              {project.type === 'web' && (
+                <Image filename={`project-slide__${project.title}.png`} alt={project.title} classes="project-slide__img web" />
+              )}
+              {project.type === 'phone' && (
+                <div className="project-slide__img phone">
+                  <Image filename={`project-slide__${project.title}-1.png`} alt={project.title} />
+                  <Image filename={`project-slide__${project.title}-2.png`} alt={project.title} />
+                  <Image filename={`project-slide__${project.title}-3.png`} alt={project.title} />
+                </div>
+              )}
+              <div className="project-slide__info">
+                <div className="project-slide__short-desc to-animate">{project.shortDescription}</div>
+                <div className="project-slide__title to-animate">{project.title}.</div>
+                <div className="project-slide__count"><span className="count">{(current.toString().length < 2) ? `0${current}` : current}.</span></div>
+                <div className={`scroll-indicator ${indicator ? '' : 'hide'}`}>
+                  <span>SCROLL</span>
+                  <div className="indicator"><span></span></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </TransitionLink>
   )
 }
