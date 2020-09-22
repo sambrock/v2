fe1645ff6d8c57534a03bdcae5154bec213d13c7
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-22 23:44:54 +0100

    Added responsive styles.

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,34 +1,49 @@
-import React, { useEffect, useState } from "react"
+import React, { createRef, useEffect, useRef, useState } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layouts/layout"
 import Image from "../components/image";
 import TechList from "../components/tech-list";
+import { useWindowSize } from "../hooks/useWindowSize";
+import { projectPageAnimateIn } from "../animations/animations";
+import ProjectImages from "../components/project-images";
 
-export default function Project({ data }) {
+
+export default function Project({ data, transitionStatus }) {
   const [project, setProject] = useState();
-
+  const [width, height] = useWindowSize();
+  
   useEffect(() => {
     setProject(data.projects.edges[0].node)
   }, [data])
 
-  if (!project) return <div></div>;
+  useEffect(() => {
+    if (transitionStatus === 'entering') projectPageAnimateIn(height).play();
+  }, [transitionStatus])
 
-  console.log(project.images);
+  if (!project) return <div></div>;
 
   return (
     <Layout>
-      <main className="project">
+      <main className={`project`}>
         <div className="project__hero" style={{ background: project.color }}>
-          <div className="testtestetestse"></div>
           <Image filename={`project__${project.title}.png`} alt={project.title} classes="project__logo" />
-          <Image filename={`project-slide__${project.title}.png`} alt={project.title} classes="project__mainimg" />
+          {project.type === 'web' && (
+            <Image filename={`project-slide__${project.title}.png`} alt={project.title} classes="project__mainimg" />
+          )}
+          {project.type === 'phone' && (
+            <div className="project__mainimg phone">
+              <Image filename={`project-slide__${project.title}-1.png`} alt={project.title} />
+              <Image filename={`project-slide__${project.title}-2.png`} alt={project.title} />
+              <Image filename={`project-slide__${project.title}-3.png`} alt={project.title} />
+            </div>
+          )}
         </div>
         <div className="project__info">
           <h2 className="project__sub-heading">{project.shortDescription}</h2>
           <p className="project__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <div className="project__info-items">
             <div className="project__info-item">
-              <h3 style={{ color: project.color }}>Year</h3>
+              <h3 style={{ color: project.color }}>Completed</h3>
               <span>{project.completed}</span>
             </div>
             <div className="project__info-item">
@@ -47,13 +62,11 @@ export default function Project({ data }) {
           <div className="project__btns">
             <a className="project__launch-btn btn" target="__blank" style={{ color: project.color, border: `1px ${project.color} solid` }} href={project.url}>Launch project</a>
             {project.github && (
-              <a href={project.github} target="__blank" className="project__github-link btn">View on GitHub</a>
+              <a href={project.github} target="__blank" className="project__github-link btn no-border">View on GitHub</a>
             )}
           </div>
         </div>
-        <div className="project__images">
-          {/* {project.images.map(i => <Image filename={i} alt={project.title} classes="project__img" />)} */}
-        </div>
+        <ProjectImages title={project.title} />
       </main>
     </Layout>
   )
@@ -73,8 +86,6 @@ export const query = graphql`
           role
           tech
           github
-          images
-          protopie
         }
       }
     }
