d1aec8c88e3f89ced5f587aa8fd55dd62eec7780
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-09-22 23:44:40 +0100

    Changed div container class name.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import { graphql } from 'gatsby';
 import React, { useState } from 'react'
+import { graphql } from 'gatsby';
 import Layout from '../components/layouts/layout';
 import Projects from '../components/projects';
 import SocialLinks from '../components/social-links';
@@ -8,22 +8,23 @@ import '../styles/main.scss';
 
 export default function Index({ data }) {
   const [accentColor, setAccentColor] = useState();
+  const isMobile = window.innerWidth < 768;
 
   return (
     <Layout>
-        <div className="container">
-          <section className="overview">
-            <div className="intro">
-              <h1>Hi, <br /> I'm Sam, <br /> web developer.</h1>
-              <p>I use modern web technologies to create engaging online
-              experiences. I'm currently a Web Technologies student at the University of
+      <div className="index">
+        <section className="overview">
+          <div className="intro">
+            <h1>Hi, <br /> I'm Sam, <br /> web developer.</h1>
+            <p>I use modern web technologies to create engaging online
+            experiences. I'm currently a Web Technologies student at the University of
             Huddersfield, graduating in 2021. </p>
-              <a className="btn primary-bg" style={{ color: accentColor, border: `1px solid ${accentColor}` }} href="mailto:sambrocklehurst1998@gmail.com">Get in touch</a>
-            </div>
-            <SocialLinks />
-          </section>
-          <Projects data={data.projects.edges} setAccent={(color) => setAccentColor(color)} />
-        </div>
+            <a className="btn primary-bg" style={{ color: accentColor, border: `1px solid ${accentColor}` }} href="mailto:sambrocklehurst1998@gmail.com">Get in touch</a>
+          </div>
+        </section>
+        <SocialLinks />
+        <Projects data={data.projects.edges} setAccent={(color) => setAccentColor(color)} isMobile={isMobile} />
+      </div>
     </Layout>
   )
 }
